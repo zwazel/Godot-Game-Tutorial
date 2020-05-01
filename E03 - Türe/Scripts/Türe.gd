39a4ab02ffd8f0d04cb5f7aac1b04545cb18c403
Tim Irmler <zwazel@hotmail.com>
2020-05-01 12:51:39 +0200

    redo E03

--- E03 - Türe/Scripts/Türe.gd
+++ E03 - Türe/Scripts/Türe.gd
@@ -1,15 +1,19 @@
-extends Position3D
+extends StaticBody
 
-onready var anim_player = $AnimationPlayer
-var state = false # false = geschlossen und true = geöffnet
+var rotatePoint
+var anim_player
+var state = false # true = offen, false = geschlossen
 
-func open(hat_key):
+func _ready():
+	rotatePoint = get_parent().get_parent()
+	anim_player = rotatePoint.get_child(0)
+
+func open_door(hat_key):
 	if hat_key:
 		if !anim_player.is_playing():
 			if state:
 				anim_player.play_backwards("Öffnen")
 				state = false
-			
 			else:
 				anim_player.play("Öffnen")
 				state = true
